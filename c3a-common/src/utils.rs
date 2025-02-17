c6664feade9076af6e44310efdff2579036756de
Klimenty Titov <aclo.create@gmail.com>
2025-02-18 02:12:58 +0300

    wip

--- c3a-common/src/utils.rs
+++ c3a-common/src/utils.rs
@@ -6,6 +6,6 @@ pub fn sign(data: &[u8], keypair: &pqc_dilithium::Keypair) -> Vec<u8> {
   keypair.sign(data).to_vec()
 }
 
-pub fn verify(data: &[u8], sign: &[u8], keypair: &pqc_dilithium::Keypair) -> bool {
-  pqc_dilithium::verify(sign, data, &keypair.public).is_ok()
+pub fn verify(data: &[u8], sign: &[u8], public_key: &[u8]) -> bool {
+  pqc_dilithium::verify(sign, data, public_key).is_ok()
 }
